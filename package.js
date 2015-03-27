f25244d62b0232259f0a0ac183baba14939a3a76
josephktcheung <josephktcheung@gmail.com>
2015-03-27 12:22:21 +0800

    Add package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: 'josephktcheung:drawer',
+  summary: 'Wraps Drawer jQuery plugin into a meteor package.',
+  version: '2.4.0',
+  git: 'https://github.com/josephktcheung/drawer.git'
+});
+
+Package.onUse(function(api) {
+  api.addFiles([
+    'drawer/dist/css/drawer.min.css',
+    'drawer/dist/js/jquery.drawer.min.css'
+    ], ['client']);
+});
